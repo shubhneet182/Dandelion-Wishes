0b04369dcde2ffe834f984bc390221dce4d4728e
Shubhneet Sandhu <shubhneet4sandhu@gmail.com>
2025-09-07 19:04:35 -0400

    New fonts and text color for wish display

--- src/app/displayWish/page.tsx
+++ src/app/displayWish/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { Doto } from "next/font/google";
+// import { Doto } from "next/font/google";
 import "./display-wish.css";
 
-const doto = Doto({
-    subsets: ["latin"],
-    weight: "900"
-})
+// const doto = Doto({
+//     subsets: ["latin"],
+//     weight: "900"
+// })
 
 const DisplayWish = () => {
     const searchParams = useSearchParams();
@@ -14,7 +14,8 @@ const DisplayWish = () => {
 
     return (
         <div className="marquee-container">
-        <p className={`${doto.className} marquee-text`}>{wish}</p>
+        {/* <p className={`${doto.className} marquee-text`}>{wish}</p> // Google Font */}
+        <p className={`marquee-text font4`}>{wish}</p>
         </div>
     )
 }
